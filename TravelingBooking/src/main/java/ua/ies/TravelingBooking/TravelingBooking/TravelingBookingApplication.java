8f8ebd6ba7465768202762fc2859bceaeb2138b7
Roberto Castro <robertorcastro@ua.pt>
2023-11-23 10:16:26 +0000

    Update

---
+++ TravelingBooking/src/main/java/ua/ies/TravelingBooking/TravelingBooking/TravelingBookingApplication.java
@@ -0,0 +1,13 @@
+package ua.ies.TravelingBooking.TravelingBooking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelingBookingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravelingBookingApplication.class, args);
+	}
+
+}
---
+++ TravelingBooking/src/test/java/ua/ies/TravelingBooking/TravelingBooking/TravelingBookingApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.ies.TravelingBooking.TravelingBooking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelingBookingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
