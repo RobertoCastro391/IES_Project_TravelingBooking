2672aee87521e31fae427cd6b18ca83ff2cf6446
Tiago Gomes <tiagocgomes@ua.pt>
2023-12-12 14:53:51 +0000

    hotels

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,28 +4,30 @@ import {
   Route,
 } from "react-router-dom";
 import Home from "./pages/home/Home";
-import Hotel from "./pages/hotel/Hotel";
-import List from "./pages/list/List";
+import Hotel from "./pages/searchHotel/Hotel";
 import Flights from "./pages/searchFlights/Flights";
 import Login from "./pages/Login/Login";
 import Register from "./pages/Register/Register";
 import Account from "./pages/Account/Account";
 import FlightCheckout from "./pages/FlightCheckout/FlightCheckout";
 import AddExtrasFlight from "./pages/AddExtrasFlight/AddExtrasFlight";
+import HotelDetails from "./pages/hotelDetails/HotelDetails";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home/>}/>
-        <Route path="/hotels" element={<List/>}/>
-        <Route path="/hotels/:id" element={<Hotel/>}/>
+        <Route path="/hotels" element={<Hotel/>}/>
         <Route path="/flights" element={<Flights/>}/>
         <Route path="/login" element={<Login/>}/>
         <Route path="/register" element={<Register/>}/>
         <Route path="/account" element={<Account/>}/>
         <Route path="/flightcheckout" element={<FlightCheckout/>}/>
         <Route path="/AddExtrasFlight" element={<AddExtrasFlight/>}/>
+        <Route path="/hotelDetails" element={<HotelDetails/>}/>
+
+
       </Routes>
     </BrowserRouter>
   );
